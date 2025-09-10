f8479f6157527f9c6240d0d6e72a629975174cd6
JulianImperiale <julian.m.imperiale@gmail.com>
2025-09-09 22:21:37 -0300

    ultimos cambios

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 include "./repository/query_pokemon.php";
+echo "<link rel='stylesheet' href='style.css'>";
 
 echo "<table>";
 foreach($traerListaPokemonsArrays as $pokemon){
     echo "<tr>";
-    echo "<td>" . $pokemon["id"] . "</td>";
-    echo "<td>" . $pokemon["name"] . "</td>";
-    echo "<td>" . $pokemon["description"] . "</td>";
-    echo "<td>" . $pokemon["type_id"] . "</td>";
-    echo "<img src='$pokemon[image_path]'> </img>";
+    echo "<td>" . $pokemon["numero_identificador"] . "</td>";
+    echo "<td>" . $pokemon["nombre"] . "</td>";
+    echo "<td>" . $pokemon["descripcion"] . "</td>";
+    echo "<img src='$pokemon[imagen_ruta]'> </img>";
+    echo "<img src='$pokemon[imagen_ruta_tipo]'> </img>";
     echo "</tr>";
 }
 echo "</table>";
--- repository/query_pokemon.php
+++ repository/query_pokemon.php
@@ -3,15 +3,24 @@ include_once("./repository/PokedexBD.php");
 $config = parse_ini_file("./config/config.ini");
 
 $conexion = new PokedexBD(
-    $config['server'], 
-    $config['username'], 
-    $config['password'], 
-    $config['database']) or die("Problemas con la conexion");
+    $config['server'],
+    $config['username'],
+    $config['password'],
+    $config['database']
+) or die("Problemas con la conexion");
 
 
 
 
-$traerListaPokemons = "SELECT * FROM pokemon";
+$traerListaPokemons = "SELECT pokemon.id AS 'numero_identificador', 
+pokemon.nombre AS 'nombre', 
+pokemon.descripcion AS 'descripcion', 
+pokemon.numero_identificador AS 'numero_identificador',
+tipos.nombre AS 'tipo',
+pokemon.imagen_ruta as 'imagen_ruta',
+tipos.imagen_ruta_tipo as 'imagen_ruta_tipo'
+FROM pokemon 
+JOIN tipos ON pokemon.tipo_id = tipos.id";
 $result = $conexion->query($traerListaPokemons);
 
 
@@ -19,4 +28,3 @@ $traerListaPokemonsArrays = $result;
 
 
 $conexion->close();
-?>
