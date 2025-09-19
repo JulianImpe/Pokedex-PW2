18da04858aa204358085c80c2bc51fa8a9e9bcf6
Gonzalo Greatti <gonza_lauty@outlook.com>
2025-09-19 19:57:11 -0300

    agrego los responsive del registrar

--- auth/registrar.php
+++ auth/registrar.php
@@ -2,7 +2,10 @@
 
 <?php
 echo "<link rel='stylesheet' href='../styles/registrar.css'>";
+echo "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
 session_start();
+
+//include('../views/layout/header.php');
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $nombre = $_POST['nombre'];
@@ -44,20 +47,44 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     }
 }
 ?>
+<div class="div-registrar">
 
-<h1>Registrarse</h1>
 
 <?php if(isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
 
+
 <form method="post" action="">
+    <h1>Registrarse</h1>
     <input type="text" name="nombre" placeholder="Nombre" required><br>
     <input type="password" name="contraseña" placeholder="Contraseña" required><br>
     <input type="password" name="nuevaContraseña" placeholder="NuevaContraseña"><br>
 
-    <input type="radio" name="rol" value="admin" >Administrador
-    <input type="radio" name="rol" value="usuario" >Usuario<br>
+
+  <!--  <input type="radio" name="rol" value="admin" class="input-radio">Administrador
+    <input type="radio" name="rol" value="usuario" class="input-radio" >Usuario<br>-->
+    <div class="rol-container">
+        <label>
+            <input type="radio" name="rol" value="admin"> Administrador
+        </label>
+        <label>
+            <input type="radio" name="rol" value="usuario"> Usuario
+        </label>
+    </div>
 
     <button>Registrarse</button>
+    <br>
+    <br>
+    <p>¿Ya tienes cuenta? <a href="login.php">Iniciar Sesión</a></p>
 </form>
 
-<p>¿Ya tienes cuenta? <a href="login.php">Iniciar Sesión</a></p>
+
+</div>
+
+
+
+<?php
+include('../views/layout/footer.php');
+echo "<link rel='stylesheet' href='../styles/footer.css'>"
+?>
+
+
